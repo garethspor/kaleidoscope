249785eff359194853e5b1a5eddab0ecdcd45cb1
garethspor <garethspor@gmail.com>
2020-12-31 09:44:09 -0800

    Initial Commit

---
+++ Kaleidoscope/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Kaleidoscope
+//
+//  Created by Gareth Spor on 12/31/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
